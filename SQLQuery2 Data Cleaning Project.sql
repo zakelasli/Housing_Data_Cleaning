64a68c30a822a0f75465f83dd891e244706e5251
zakelasli <149749616+zakelasli@users.noreply.github.com>
2023-11-04 02:17:58 +0100

    Update SQLQuery2 Data Cleaning Project.sql

--- SQLQuery2 Data Cleaning Project.sql
+++ SQLQuery2 Data Cleaning Project.sql
@@ -1,96 +1,93 @@
---Select * 
---From [Portfolio Project].dbo.NashvilleHousing
+Select * 
+From [Portfolio Project].dbo.NashvilleHousing
+-- standardizing date format
+Select SaleDateConverted, Convert(Date, Saledate)
+From [Portfolio Project].dbo.NashvilleHousing
 
---Select SaleDateConverted, Convert(Date, Saledate)
---From [Portfolio Project].dbo.NashvilleHousing
+Update NashvilleHousing
+Set SaleDate = Convert(Date, Saledate)
 
---Update NashvilleHousing
---Set SaleDate = Convert(Date, Saledate)
+Alter Table NashvilleHousing
+Add SaleDateConverted Date; 
 
---Alter Table NashvilleHousing
---Add SaleDateConverted Date; 
+Update NashvilleHousing
+Set SaleDateConverted = Convert(Date, Saledate)
 
---Update NashvilleHousing
---Set SaleDateConverted = Convert(Date, Saledate)
+-- same ParcelID has the same address, so we will use it to fill in the address
+Select  a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, Isnull(a.PropertyAddress, b.PropertyAddress)
+From [Portfolio Project].dbo.NashvilleHousing a 
+Join [Portfolio Project].dbo.NashvilleHousing b 
+ON a.ParcelID = b.ParcelID
+And a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress is null  
 
---Select  a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, Isnull(a.PropertyAddress, b.PropertyAddress)
---From [Portfolio Project].dbo.NashvilleHousing a 
---Join [Portfolio Project].dbo.NashvilleHousing b 
---ON a.ParcelID = b.ParcelID
---And a.[UniqueID ] <> b.[UniqueID ]
---Where a.PropertyAddress is null  
+Update a
+SET PropertyAddress =  Isnull(a.PropertyAddress, b.PropertyAddress)
+From [Portfolio Project].dbo.NashvilleHousing a 
+Join [Portfolio Project].dbo.NashvilleHousing b 
+ON a.ParcelID = b.ParcelID
+And a.[UniqueID ] <> b.[UniqueID ]
 
---Update a
---SET PropertyAddress =  Isnull(a.PropertyAddress, b.PropertyAddress)
---From [Portfolio Project].dbo.NashvilleHousing a 
---Join [Portfolio Project].dbo.NashvilleHousing b 
---ON a.ParcelID = b.ParcelID
---And a.[UniqueID ] <> b.[UniqueID ]
+-- splitting address into different columns (address, city, state)
+-- for PropertyAddress
+Select 
+PARSENAME (REplace(OwnerAddress, ',', '.'), 3) AS StreetAddress,
+PARSENAME (REplace(OwnerAddress, ',', '.'), 2) As City,
+PARSENAME (REplace(OwnerAddress, ',', '.'), 1) AS State 
+FRom [Portfolio Project].dbo.NashvilleHousing; 
 
---Select 
---PARSENAME (REplace(OwnerAddress, ',', '.'), 3) AS StreetAddress,
---PARSENAME (REplace(OwnerAddress, ',', '.'), 2) As City,
---PARSENAME (REplace(OwnerAddress, ',', '.'), 1) AS State 
---FRom [Portfolio Project].dbo.NashvilleHousing; 
+ALTER TABLE [Portfolio Project].dbo.NashvilleHousing
+ADD OwnerSplitAddress Nvarchar(255)
 
---ALTER TABLE [Portfolio Project].dbo.NashvilleHousing
---ADD OwnerSplitAddress Nvarchar(255)
+Update [Portfolio Project].dbo.NashvilleHousing
+SET OwnerSplitAddress = PARSENAME (REplace(OwnerAddress, ',', '.'), 3)  
 
---Update [Portfolio Project].dbo.NashvilleHousing
---SET OwnerSplitAddress = PARSENAME (REplace(OwnerAddress, ',', '.'), 3)  
+ALTER TABLE [Portfolio Project].dbo.NashvilleHousing
+ADD OwnerSplitCity Nvarchar(255)
 
---ALTER TABLE [Portfolio Project].dbo.NashvilleHousing
---ADD OwnerSplitCity Nvarchar(255)
+Update [Portfolio Project].dbo.NashvilleHousing
+SET OwnerSplitAddress = PARSENAME (REplace(OwnerAddress, ',', '.'), 2)  
 
---Update [Portfolio Project].dbo.NashvilleHousing
---SET OwnerSplitAddress = PARSENAME (REplace(OwnerAddress, ',', '.'), 2)  
+ALTER TABLE [Portfolio Project].dbo.NashvilleHousing
+ADD OwnerSplitState Nvarchar(255)
 
---ALTER TABLE [Portfolio Project].dbo.NashvilleHousing
---ADD OwnerSplitState Nvarchar(255)
+Update [Portfolio Project].dbo.NashvilleHousing
+SET OwnerSplitState = PARSENAME (REplace(OwnerAddress, ',', '.'), 1)  
 
---Update [Portfolio Project].dbo.NashvilleHousing
---SET OwnerSplitState = PARSENAME (REplace(OwnerAddress, ',', '.'), 1)  
+Select * 
+From [Portfolio Project].dbo.NashvilleHousing
+-- changing 'Y' and 'N' to 'Yes' and 'No'
+Select Distinct SoldAsVacant, Count(SoldAsVacant)
+From [Portfolio Project].dbo.NashvilleHousing
+Group by SoldAsVacant
+Order by 2; 
 
---Select * 
---From [Portfolio Project].dbo.NashvilleHousing
+Select SoldAsVacant 
+,Case When SoldAsVacant = 'Y' Then 'YES'
+      When SoldAsVacant = 'N' Then 'No'
+	  Else SoldAsVacant
+	  END
+From [Portfolio Project].dbo.NashvilleHousing 
 
---Select Distinct SoldAsVacant, Count(SoldAsVacant)
---From [Portfolio Project].dbo.NashvilleHousing
---Group by SoldAsVacant
---Order by 2; 
-
---Select SoldAsVacant 
---,Case When SoldAsVacant = 'Y' Then 'YES'
---      When SoldAsVacant = 'N' Then 'No'
---	  Else SoldAsVacant
---	  END
---From [Portfolio Project].dbo.NashvilleHousing 
-
---Update [Portfolio Project].dbo.NashvilleHousing
---SET SoldAsVacant = Case When SoldAsVacant = 'Y' Then 'YES'
---      When SoldAsVacant = 'N' Then 'No'
---	  Else SoldAsVacant
---	  END
---SELECT * 
---FROM [Portfolio Project].dbo.NashvilleHousing
-
---WITH RowNumCTE AS (
---    SELECT
---        ROW_NUMBER() OVER (PARTITION BY  ParcelID,
---        PropertyAddress,
---		SalePrice,
---		SaleDate,
---		LegalReference ORDER BY (SELECT 1)) AS RowNum
---    FROM [Portfolio Project].dbo.NashvilleHousing
---)
---Select *
---FROM RowNumCTE WHERE RowNum > 1;
+Update [Portfolio Project].dbo.NashvilleHousing
+SET SoldAsVacant = Case When SoldAsVacant = 'Y' Then 'YES'
+      When SoldAsVacant = 'N' Then 'No'
+	  Else SoldAsVacant
+	  END
+SELECT * 
+FROM [Portfolio Project].dbo.NashvilleHousing
+-- removing duplicates
+WITH RowNumCTE AS (
+    SELECT
+        ROW_NUMBER() OVER (PARTITION BY  ParcelID,
+       PropertyAddress,
+		SalePrice,
+		SaleDate,
+		LegalReference ORDER BY (SELECT 1)) AS RowNum
+    FROM [Portfolio Project].dbo.NashvilleHousing
+)
+Select *
+FROM RowNumCTE WHERE RowNum > 1;
 
 SELECT * 
 FROM [Portfolio Project].dbo.NashvilleHousing
-
-
-
-
-
-
